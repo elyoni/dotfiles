78057c286967e157bb09536c23d85a46c1e6948b
Yoni Elentok <elyoni@gmail.com>
2017-08-22 21:11:52 +0300

    First Upload

---
+++ apt-get_app.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#=== Pip pyton ===
+sudo apt-get instal python-pip -y
+pip install --upgrade pip
+
+
+#=== Install Arduino ===
+## After extracting the zip file enter the library and run ./install.sh
+# Fix port permission
+## sudo usermod -a -G dialout <username>
+sudo usermod -a -G dialout johnny
+# Add ESP8266 Support
+## Enter the flowing link http://arduino.esp8266.com/stable/package_esp8266com_index.json
+
+
+## Install Fritzing 
+sudo apt-get install fritzing -y
+
+#=== Libre Office ===
+## to install libre office we need to install aptitude
+sudo apt install aptitude -y
+sudo aptitude install libreoffice -y
+
+#=== neovim ===
+## Installation
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim
+pip install --user --upgrade neovim
+
+## To connect the clipboard of the neovim and the linux
+sudo apt-get install xclip -y
+
+#=== Install double commander ===
+sudo apt-get install doublecmd-qt -y
+
+
+
+#=== Multimedia Software ===
+## KDE N Live
+sudo apt-get install kdenlive -y
+## Mp3 player
+#sudo apt-get install rhythmbox -y # I have tried and it wan't work good
+sudo apt-get install clementine -y
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "# ================================"
+echo "# Installing the KDE settings"
+echo "# ================================"
+
+echo "Copy my kde keyboard settings"
+cp KDESettings/kglobalshortcutsrc $HOME/.config/
+echo "Copy input settings"
+cp KDESettings/kcminputrc $HOME/.kde/share/config/
+echo "Copy KDE UI color"
+cp KDESettings/kdeglobals $HOME/.kde/share/config/
+cp KDESettings/plasma-org.kde.plasma.desktop-appletsrc $HOME/.config/
+cp KDESettings/plasmashellrc $HOME/.config/
+cp KDESettings/plasma-org.kde.plasma.desktop-appletsrc $HOME/.config/
+echo "Copy Dolphon settings"
+cp KDESettings/.directory $HOME/
+echo "Copy widgets settings"
+cp KDESettings/.gtkrc-2.0 $HOME/
+cp KDESettings/gtkrc $HOME/.config/
+echo "Copy kate settings"
+cp KDESettings/katepartrc $HOME/.config
+echo "Copy kwin settings"
+cp KDESettings/kwinrc $HOME/.config
