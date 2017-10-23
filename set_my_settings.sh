544b2da73032fda44ac01285e01034038190da92
Yoni Elentok <elyoni@gmail.com>
2017-10-23 12:34:33 +0300

    change file names

--- set_my_settings.sh
+++ set_my_settings.sh
@@ -28,16 +28,25 @@ echo "Copy KDE UI color"
 cp KDESettings/kdeglobals $HOME/.kde/share/config/
 cp KDESettings/plasma-org.kde.plasma.desktop-appletsrc $HOME/.config/
 cp KDESettings/plasmashellrc $HOME/.config/
+
 echo "Copy Dolphon settings"
 cp KDESettings/.directory $HOME/
+
 echo "Copy widgets settings"
 cp KDESettings/.gtkrc-2.0 $HOME/
 cp KDESettings/gtkrc $HOME/.config/
+
 echo "Copy kate settings"
 cp KDESettings/katepartrc $HOME/.config
+
 echo "Copy kwin settings"
 cp KDESettings/kwinrc $HOME/.config
+
 echo "Install Double Commander"
 sh DoubleCommander/install.sh
+
+echo "Install VIM & VIMRC"
+sh vim/install.sh
+
 echo "TMUX Configurion"
 cp tmux/.tmux.conf $HOME/
