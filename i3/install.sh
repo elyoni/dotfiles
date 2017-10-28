c247a755d3e02ad1b582b1c965014a51458fe7de
Yoni Elentok <elyoni@gmail.com>
2017-10-28 10:04:06 +0300

    combine i3 settings with the laptop

---
+++ i3/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# ===== Install moduls for i3 =====
+# Install an alternative for i3status
+sudo apt-get install i3blocks -y
+
+# Graphic application finder, A Graphic replace for dmenu
+sudo apt-get install xfce4-appfinder -y
+
+# Test application finder, A test replace for dmenu
+sudo apt-get install j4-dmenu-desktop -y
+
+
