f1ded93210f6435f8135b51c8c58967aadf94bd5
Yoni Elentok <elyoni@gmail.com>
2017-10-28 14:04:10 +0300

    Fix lang. switch and monitor sides

--- i3/install.sh
+++ i3/install.sh
@@ -9,4 +9,17 @@ sudo apt-get install xfce4-appfinder -y
 # Test application finder, A test replace for dmenu
 sudo apt-get install j4-dmenu-desktop -y
 
+# Text folder size viewer
+sudo apt-get install gt5 -y
 
+# Network Manaer
+sudo apt-get install network-manager -y
+
+# Volume Controller
+sudo apt-get install pnmixer -y 
+
+rm $HOME/.config/i3/config
+ln -s $(pwd)/i3/config $HOME/.config/i3/config
+
+rm $HOME/.i3blocks.conf
+ln -s $(pwd)/i3/i3blocks.conf $HOME/.i3blocks.conf
