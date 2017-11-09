ee5380b7a7fc9a77daecdd22dd7811f0f3e3cbad
Yoni Elentok <elyoni@gmail.com>
2017-11-09 07:55:01 +0200

    Fix Num-lock Caps-lock notification + added workgroup names

--- DoubleCommander/install.sh
+++ DoubleCommander/install.sh
@@ -4,5 +4,5 @@
 sudo apt-get install doublecmd-qt -y
 
 echo "Double Commander Settings"
-ln -s $(pwd)/DoubleCommander/doublecmd.xml $HOME/.config/doublecmd/doublecmd.xml
-ln -s $(pwd)/DoubleCommander/shortcuts.scf $HOME/.config/doublecmd/shortcuts.scf
+ln -sf $(pwd)/DoubleCommander/doublecmd.xml $HOME/.config/doublecmd/doublecmd.xml
+ln -sf $(pwd)/DoubleCommander/shortcuts.scf $HOME/.config/doublecmd/shortcuts.scf
