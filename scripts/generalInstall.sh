cbe10e18189e456a1e246beee560d03cc137d225
Yoni Elentok <elyoni@gmail.com>
2017-11-09 10:37:54 +0200

    Download bluetooth programs

--- scripts/generalInstall.sh
+++ scripts/generalInstall.sh
@@ -28,3 +28,13 @@ bash DoubleCommander/install.sh
 
 echo "Install tmux"
 bash tmux/install.sh
+
+# === bluetooth === 
+sudo apt-get install bluetooth bluez bluez-tools rfkill -y
+# TO check if the bluetooth is blocked we can run
+## code: sudo rfkill list
+## if your device is blocked you can run
+## code: sudo rfkill unblock bluetooth
+## to start the bluetooth service run:
+## code: sudo service bluetooth start
+sudo apt-get install blueman -y
