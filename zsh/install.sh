eab99588e74dea6e528a87c40bf4712bbbb20829
Yoni Elentok <yehonatan.e@solaredge.com>
2018-03-29 23:27:10 +0300

    Add on my zsh intall

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,5 +3,7 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 ln -sf $DIR/.zshrc $HOME/.zshrc
 ln -sf $DIR/yoni.zsh-theme $HOME/.oh-my-zsh/themes/yoni.zsh-theme
