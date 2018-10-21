0daf66adc0891790575cf5486e87b454045801a3
Yoni Elentok <yehonatan.e@solaredge.com>
2018-10-21 22:57:11 +0300

    Add zsh to installer

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,6 +3,7 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
+sudo apt-get install zsh -y
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 ln -sf $DIR/.zshrc $HOME/.zshrc
