ef79f55706fb1bbcddf8e23621112a47be3ba7a8
Yoni Elentok <yehonatan.e@solaredge.com>
2019-06-27 22:05:10 +0300

    Add link to ranger installer

--- ranger/install.sh
+++ ranger/install.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+DIR=$(dirname "${BASH_SOURCE[0]}")
+DIR=$(cd -P $DIR && pwd)
+
 sudo apt-get install xsel
+ln -sf $DIR/rc.conf $HOME/.config/ranger/rc.conf
+ln -sf $DIR/rifle.conf $HOME/.config/ranger/rifle.conf
+ln -sf $DIR/scope.sh $HOME/.config/ranger/scope.sh
+echo Done
