badf9e715c7888dec313ad10d61d5ccb9f410957
Yoni Elentok <yehonatan.e@solaredge.com>
2019-07-11 10:32:49 +0300

    Add dialog to apt-get

--- ranger/install.sh
+++ ranger/install.sh
@@ -3,6 +3,7 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
 sudo apt-get install xsel
+sudo apt-get install dialog
 ln -sf $DIR/rc.conf $HOME/.config/ranger/rc.conf
 ln -sf $DIR/rifle.conf $HOME/.config/ranger/rifle.conf
 ln -sf $DIR/scope.sh $HOME/.config/ranger/scope.sh
