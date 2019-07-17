936cef02292a5c341a26c79b221be0c41b38383d
Yoni Elentok <yehonatan.e@solaredge.com>
2019-07-17 21:44:31 +0300

    Improved the install script

--- tig/install.sh
+++ tig/install.sh
@@ -10,10 +10,13 @@ sudo apt-get install unzip -y
 
 
 mkdir ~/Downloads/tig
-wget -P ~/Downloads/tig "https://github.com/jonas/tig/archive/master.zip"
-unzip ~/Downloads/tig/master.zip -d ~/Downloads/tig/
-cd ~/Downloads/tig/tig-master/
-make prefix=/usr/local
-sudo make install prefix=/usr/local
+#wget -P ~/Downloads/tig "https://github.com/jonas/tig/archive/master.zip"
+wget -P ~/Downloads/tig "https://github.com/jonas/tig/archive/tig-2.4.1.zip"
+unzip ~/Downloads/tig/tig-2.4.1.zip -d ~/Downloads/tig/
+cd ~/Downloads/tig/tig-2.4.1/
+make
+sudo make install
+#make prefix=/usr/local
+#sudo make install prefix=/usr/local
 
 $DIR/install_link.sh
