2597c2514f3569e221b67d4cb2c3f4e1ce27c765
Yoni Elentok <yehonatan.e@solaredge.com>
2019-11-14 16:38:17 +0200

    Fix tig install file

--- tig/install.sh
+++ tig/install.sh
@@ -3,17 +3,17 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
-#sudo apt-get install tig -y
 
 #Install the new version
 sudo apt-get install unzip -y
+wget -P ~/Downloads/tig "https://github.com/jonas/tig/archive/tig-2.4.1.zip"
 
 
 mkdir ~/Downloads/tig
 #wget -P ~/Downloads/tig "https://github.com/jonas/tig/archive/master.zip"
 wget -P ~/Downloads/tig "https://github.com/jonas/tig/archive/tig-2.4.1.zip"
-unzip ~/Downloads/tig/tig-2.4.1.zip -d ~/Downloads/tig/
-cd ~/Downloads/tig/tig-2.4.1/
+unzip ~/Downloads/tig/tig-tig-2.4.1.zip -d ~/Downloads/tig/
+cd ~/Downloads/tig/tig-tig-2.4.1/
 make
 sudo make install
 #make prefix=/usr/local
