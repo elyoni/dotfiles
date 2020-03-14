0537597f60ad5fb00651eecd8e58f301a0d3f935
Yoni Elentok <yehonatan.e@solaredge.com>
2020-03-14 22:47:12 +0200

    Update tig install file, ver 2.5.0

--- tig/install.sh
+++ tig/install.sh
@@ -4,19 +4,32 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
 #sudo apt-get install tig -y
+function link_files()
+{
+    echo ======= link_files =======
+    ln -sf $DIR/tigrc $HOME/.tigrc
+    echo =======    end    =======
+}
 
-#Install the new version
-sudo apt-get install unzip -y
+function install_tig()
+{
+    VERSION="tig-2.5.0" 
+    echo ======= install tig Version $VERSION  =======
+    DOWNLOAD_PATH=$HOME/Downloads/apps/tig
+    mkdir -p ${DOWNLOAD_PATH}
+    wget -P ${DOWNLOAD_PATH} https://github.com/jonas/tig/releases/download/$VERSION/$VERSION.tar.gz
+    tar -C ${DOWNLOAD_PATH} -xvf $DOWNLOAD_PATH/${VERSION}.tar.gz
+    cd ${DOWNLOAD_PATH}/${VERSION}
+    make prefix=/usr/local
+    sudo make install prefix=/usr/local
+    cd -
+    echo ======= End  =======
+}
 
+function install()
+{
+    install_tig
+    link_files
+}
 
-mkdir ~/Downloads/tig
-#wget -P ~/Downloads/tig "https://github.com/jonas/tig/archive/master.zip"
-wget -P ~/Downloads/tig "https://github.com/jonas/tig/archive/tig-2.4.1.zip"
-unzip ~/Downloads/tig/tig-2.4.1.zip -d ~/Downloads/tig/
-cd ~/Downloads/tig/tig-2.4.1/
-make
-sudo make install
-#make prefix=/usr/local
-#sudo make install prefix=/usr/local
-
-$DIR/install_link.sh
+"$@" 
