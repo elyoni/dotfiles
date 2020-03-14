a92a437c1ba3e07d0f8884c4cab9792988c8688f
Yoni Elentok <yehonatan.e@solaredge.com>
2020-03-14 22:47:29 +0200

    Update tmux install file

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
+function link_files()
+{
+    echo ======= Link Files =======
+    ln -sf $DIR/tmux.conf $HOME/.tmux.conf
+    echo =======   End   =======
+}
 
-sudo apt-get install tmux -y
-sudo apt-get install jq -y
+function install_tmux()
+{
+    echo ======= Install Tmux =======
+    sudo apt-get install tmux -y
+    sudo apt-get install jq -y
+    echo ======= End  =======
+}
 
-$DIR/install_link.sh
+function install()
+{
+    install_tmux
+    link_files
+}
+
+"$@" 
