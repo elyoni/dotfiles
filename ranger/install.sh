9a93a4cbe3abbdb27a6cd20a7ac3e6e8c6d27b90
Yoni Elentok <yehonatan.e@solaredge.com>
2020-03-14 22:47:00 +0200

    Update ranger install file

--- ranger/install.sh
+++ ranger/install.sh
@@ -2,9 +2,30 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
-sudo apt-get install xsel
-sudo apt-get install dialog
-ln -sf $DIR/rc.conf $HOME/.config/ranger/rc.conf
-ln -sf $DIR/rifle.conf $HOME/.config/ranger/rifle.conf
-ln -sf $DIR/scope.sh $HOME/.config/ranger/scope.sh
-echo Done
+
+function install_application()
+{
+    echo ====== Install Ranger + Packages =====
+    sudo apt-get install ranger -y
+    sudo apt-get install xsel -y
+    sudo apt-get install dialog -y
+    sudo apt-get install file -y
+    echo ====== End =====
+}
+
+function link_files()
+{
+    echo ======= Link Files =======
+    ln -sf $DIR/rc.conf $HOME/.config/ranger/rc.conf
+    ln -sf $DIR/rifle.conf $HOME/.config/ranger/rifle.conf
+    ln -sf $DIR/scope.sh $HOME/.config/ranger/scope.sh
+    echo =======   End   =======
+}
+
+function install()
+{
+    install_application
+    link_files
+}
+
+"$@"
