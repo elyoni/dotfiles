d6d99bdf954492e35bb02474b9002b7e88e40ed9
Yoni Elentok <yehonatan.e@solaredge.com>
2020-03-14 22:46:35 +0200

    Update i3 install script

--- i3/install.sh
+++ i3/install.sh
@@ -2,45 +2,64 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
+function application_install()
+{
+    echo ====== Start Application install =======
+    echo ====== Install i3 =======
+    sudo apt-get install i3 -y
 
-mkdir $HOME/.config/i3/scripts
+    # ===== Install modules for i3 =====
+    # Install an alternative for i3status
+    echo ====== Install i3blocks =======
+    sudo apt-get install i3blocks -y
 
-sudo apt-get install i3 -y
+    # Graphic application finder, A Graphic replace for dmenu
+    echo ====== Install xfce4-appfinder =======
+    sudo apt-get install xfce4-appfinder -y
 
-# ===== Install moduls for i3 =====
-# Install an alternative for i3status
-sudo apt-get install i3blocks -y
+    # Test application finder, A test replace for dmenu
+    #echo ====== Install j4-dmenu-desktop =======
+    #sudo apt-get install j4-dmenu-desktop -y
 
-# Graphic application finder, A Graphic replace for dmenu
-sudo apt-get install xfce4-appfinder -y
+    # Text folder size viewer
+    sudo apt-get install gt5 -y
 
-# Test application finder, A test replace for dmenu
-sudo apt-get install j4-dmenu-desktop -y
+    # Network Manager
+    echo ====== Install network-manager =======
+    sudo apt-get install network-manager -y
 
-# Text folder size viewer
-sudo apt-get install gt5 -y
+    echo ====== Install feh =======
+    sudo apt-get install feh -y
 
-# Network Manaer
-sudo apt-get install network-manager -y
+    # Screen manager
+    echo ====== Install arandr =======
+    sudo apt-get install arandr -y 
 
-sudo apt-get install feh -y
+    # Volume Controller
+    echo ====== Install pnmixer =======
+    sudo apt-get install pnmixer -y 
 
-# Screen manager
-sudo apt-get install arandr -y 
+    gsettings set org.gnome.desktop.background show-desktop-icons false
+}
 
-# Volume Controller
-sudo apt-get install pnmixer -y 
-ln -sf $DIR/scripts/volume.sh   $HOME/.config/i3/scripts/volume.sh
+function link_files()
+{
+    mkdir -p $HOME/.config/i3/scripts
 
-ln -sf $DIR/scripts/xrander_script.sh $HOME/.config/i3/scripts/xrander_script.sh
-ln -sf $DIR/scripts/lang.sh $HOME/.config/i3/scripts/lang.sh
+    ln -sf $DIR/scripts/volume.sh   $HOME/.config/i3/scripts/volume.sh
 
-rm $HOME/.config/i3/config
-ln -sf $DIR/config $HOME/.config/i3/config
+    ln -sf $DIR/scripts/xrander_script.sh $HOME/.config/i3/scripts/xrander_script.sh
+    ln -sf $DIR/scripts/lang.sh $HOME/.config/i3/scripts/lang.sh
 
-rm $HOME/.i3blocks.conf
-ln -sf $DIR/i3blocks.conf $HOME/.i3blocks.conf
+    ln -sf $DIR/config $HOME/.config/i3/config
 
-bash $DIR/i3blocks/scripts/idle_time/install.sh
+    ln -sf $DIR/i3blocks.conf $HOME/.i3blocks.conf
+}
 
-gsettings set org.gnome.desktop.background show-desktop-icons false
+function install()
+{
+    application_install
+    link_files
+}
+
+"$@"
