ebfdd27ed1c18f30ff6f2a27a1433cc6e2833972
Yoni Elentok <yehonatan.e@solaredge.com>
2020-03-16 22:01:42 +0200

    Add davmail install and software install

---
+++ mail/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DIR=$(dirname "${BASH_SOURCE[0]}")
+DIR=$(cd -P $DIR && pwd)
+
+function install_davmail()
+{
+    sudo apt-get install default-jre -y
+    sudo dpkg -i $DIR/davmail_5.4.0-3135-1_all.deb 
+}
+
+function install_evolution()
+{
+    sudo add-apt-repository ppa:gnome3-team/gnome3-staging
+    sudo apt-get update
+    sudo apt-get install evolution
+}
+
+function remove_evolution()
+{
+    ppa-purge gnome3-team/gnome3-staging
+    ppa-purge gnome3-team/gnome3
+}
+
+function install()
+{
+    install_davmail
+}
+
+"$@"
