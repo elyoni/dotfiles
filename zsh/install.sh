ec0918378efcb266c5c6aaac2680293c5ea02e93
Yoni Elentok <yehonatan.e@solaredge.com>
2020-03-16 21:10:22 +0200

    Add plugins to zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,6 +3,27 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
-sudo apt-get install zsh -y
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-$DIR/install_link.sh
+function install_zsh()
+{
+    sudo apt-get install zsh -y
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+}
+
+function link_files()
+{
+    echo ======= Link Files =======
+    ln -sf $DIR/.zshrc $HOME/.zshrc
+    ln -sf $DIR/yoni.zsh-theme $HOME/.oh-my-zsh/themes/yoni.zsh-theme
+    echo =======   End   =======
+}
+
+function install()
+{
+    install_zsh
+    link_files
+}
+
+"$@"
--- zsh/install_link.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR=$(dirname "${BASH_SOURCE[0]}")
-DIR=$(cd -P $DIR && pwd)
-
-ln -sf $DIR/.zshrc $HOME/.zshrc
-ln -sf $DIR/yoni.zsh-theme $HOME/.oh-my-zsh/themes/yoni.zsh-theme
