7f3517de647c022e58eb99da4431f64a279a04b5
Yoni Elentok <yehonatan.e@solaredge.com>
2020-04-20 09:00:51 +0300

    Add plugins and update the install script

--- tmux/install.sh
+++ tmux/install.sh
@@ -8,12 +8,27 @@ function link_files()
     echo =======   End   =======
 }
 
-function install_tmux()
+function install_tmux_apt_get()
 {
     echo ======= Install Tmux =======
     sudo apt-get install tmux -y
     sudo apt-get install jq -y
     echo ======= End  =======
+}
+
+function install_tmux_last()
+{
+    sudo apt-get install libevent-dev ncurses-dev build-essential bison pkg-config -y
+    DOWNLOAD_PATH=$HOME/Downloads/apps/tmux
+    VERSION="3.1-rc4"
+    VERSION_ONLY="3.1"
+    mkdir -p ${DOWNLOAD_PATH}
+    wget -P ${DOWNLOAD_PATH} https://github.com/tmux/tmux/releases/download/${VERSION_ONLY}/tmux-${VERSION}.tar.gz
+    tar -C ${DOWNLOAD_PATH} -zxf $DOWNLOAD_PATH/tmux-${VERSION}.tar.gz
+    cd ${DOWNLOAD_PATH}/tmux-${VERSION}
+    ./configure
+    make && sudo make install
+    cd -
 }
 
 function install()
