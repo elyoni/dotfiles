e4c91e6a35b624e46b22da8e0a63c65a9b107d9b
Yoni Elentok <yehonatan.e@solaredge.com>
2020-05-18 08:58:23 +0300

    Update i3 config + scripts

--- i3/scripts/backlight_brightness.sh
+++ i3/scripts/backlight_brightness.sh
@@ -5,21 +5,25 @@ basedir="/sys/class/backlight/"
 
 # get the backlight handler
 handler=$basedir$(ls $basedir)"/"
+echo $handler
 
 # get current brightness
 old_brightness=$(cat $handler"brightness")
 
 # get max brightness
 max_brightness=$(cat $handler"max_brightness")
+echo max_brightness $max_brightness
 
 # get current brightness %
 old_brightness_p=$(( 100 * $old_brightness / $max_brightness ))
+echo old_brightness_p $old_brightness_p
 
 # calculate new brightness % 
 new_brightness_p=$(($old_brightness_p $1))
 
 # calculate new brightness value
 new_brightness=$(( $max_brightness * $new_brightness_p / 100 ))
+echo $new_brightness
 
 if [ $new_brightness -gt $max_brightness ]; then
     new_brightness=${max_brightness}
