1ef77bfd99b1e3fcae56a52efbda21ad2bc876aa
Yehonatan Elentok <yehonatan.e@solaredge.com>
2022-11-15 10:06:09 +0000

    [ZSH] Reorder the paths

--- zsh/zsh_config/path.zsh
+++ zsh/zsh_config/path.zsh
@@ -1,4 +1,14 @@
+function set_other_path(){
+    local gopath="$HOME/.local/go"
+    if [[ "${gopath}" != "*${GOPATH}*" ]]; then
+        export GOPATH="${gopath}"
+    fi
 
+    local pyenv="$HOME/.pyenv"
+    if [[ "${pyenv}" != "*${PYENV_ROOT}*" ]]; then
+        export PYENV_ROOT="${pyenv}"
+    fi
+}
 function set_path(){
     local new_path="$1"
     if [ -d $new_path ]; then
@@ -9,8 +19,12 @@ function set_path(){
 
 function main(){
     set_path "/usr/local/go/bin"
+    set_path "${GOPATH}/bin"
     set_path "/usr/local/mvn/bin"
-    set_path "$HOME/.local/bin/balena-cli"
+    set_path "${HOME}/.local/bin/balena-cli"
+    set_path "${HOME}/.local/bin"
+    set_path "${PYENV_ROOT}/bin"
 }
 
+set_other_path
 main
