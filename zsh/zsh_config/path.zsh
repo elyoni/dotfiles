2757051ea6642e7edd7f105d8e7d2fb068bf16d1
Yehonatan Elentok <yehonatan.e@solaredge.com>
2022-11-15 09:56:12 +0000

    [ZSH] Add balena application to path

--- zsh/zsh_config/path.zsh
+++ zsh/zsh_config/path.zsh
@@ -1,8 +1,8 @@
 
-
 function set_path(){
     local new_path="$1"
     if [ -d $new_path ]; then
+        [ $ZSH_DEBUG ] && echo "Add ${new_path} to PATH"
         export PATH=$new_path:$PATH
     fi
 }
@@ -10,7 +10,7 @@ function set_path(){
 function main(){
     set_path "/usr/local/go/bin"
     set_path "/usr/local/mvn/bin"
+    set_path "$HOME/.local/bin/balena-cli"
 }
-
 
 main
