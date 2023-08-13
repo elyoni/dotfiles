6dc9e6af85be459147226c0efd698943bc70197c
Yehonatan Elentok <yehonatan.e@solaredge.com>
2023-08-13 10:09:27 +0300

    [Nvim] Add flash plugin

--- neovim/nvim/lua/plugins.lua
+++ neovim/nvim/lua/plugins.lua
@@ -145,6 +145,21 @@ require("lazy").setup({
         'will133/vim-dirdiff',
     },
     {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            --{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+    },
+
+    {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         dependencies = {
