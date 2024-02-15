ea0b3aba8b399b24e05c21f82b6080ae866e3620
Yehonatan Elentok <elyoni@gmail.com>
2024-02-16 00:00:40 +0200

    [Zsh] Update install the some configuration

---
+++ applications/zsh/zsh_config/keybinds.zsh
@@ -0,0 +1,10 @@
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
+
+source ~/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh
+
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
+
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
