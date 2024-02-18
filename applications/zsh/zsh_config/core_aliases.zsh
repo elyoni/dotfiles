8675abee7b3621efd3e2f97ce4c2b50565cf56c2
Yehonatan Elentok <elyoni@gmail.com>
2024-02-18 10:05:15 +0200

    [Zsh] Update

--- applications/zsh/zsh_config/core_aliases.zsh
+++ applications/zsh/zsh_config/core_aliases.zsh
@@ -6,3 +6,8 @@ alias ....='cd ../../..'
 alias l='ls -lah --color=auto'
 alias ls='ls --color=auto'
 alias ll='ls --color=auto'
+
+alias rr="ranger"
+
+alias clp='xclip -sel clip'
+alias pwdcp='pwd | xclip -sel clip'
