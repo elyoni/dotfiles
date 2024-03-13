5464a8055b4c15182ddb403b5ae0580bb419916f
Yehonatan Elentok <elyoni@gmail.com>
2024-03-13 12:55:48 +0200

    [Zsh] Add alias for gitui

--- applications/zsh/zsh_config/core_aliases.zsh
+++ applications/zsh/zsh_config/core_aliases.zsh
@@ -7,7 +7,8 @@ alias l='ls -lah --color=auto'
 alias ls='ls --color=auto'
 alias ll='ls --color=auto'
 
-alias rr="ranger"
+[ -f $(ranger) ] && alias rr="ranger"
+[ -f $(gitui) ] && alias gi="gitui"
 
 alias clp='xclip -sel clip'
 alias pwdcp='pwd | xclip -sel clip'
