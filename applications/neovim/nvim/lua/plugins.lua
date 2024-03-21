2bf58abd5dee94cfb84b51a5f4229d7df264be8e
Yehonatan Elentok <elyoni@gmail.com>
2024-03-21 14:42:03 +0200

    [Nvim] Update lazy-lock + add Obsidian plugin

--- applications/neovim/nvim/lua/plugins.lua
+++ applications/neovim/nvim/lua/plugins.lua
@@ -108,6 +108,14 @@ require("lazy").setup({
     --'farseer90718/vim-taskwarrior',
     --},
     --},
+    --
+    {
+        'nvim-pack/nvim-spectre',
+        dependencies = {
+            'nvim-lua/plenary.nvim'
+        }
+
+    },
     {
         'nvim-tree/nvim-tree.lua',
         dependencies = {
@@ -324,9 +332,7 @@ require("lazy").setup({
                 pickers = {
                     live_grep = {
                         file_ignore_patterns = { 'node_modules', '.git', '.venv' },
-                        additional_args = function(_)
-                            return { "--hidden" }
-                        end
+                        additional_args = { '--hidden' },
                     },
                     find_files = {
                         file_ignore_patterns = { 'node_modules', '.git', '.venv' },
@@ -649,6 +655,37 @@ require("lazy").setup({
         },
         opts = {
             -- configurations go here
+        },
+    },
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*", -- recommended, use latest release instead of latest commit
+        lazy = true,
+        ft = "markdown",
+        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+        -- event = {
+        --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+        --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+        --   "BufReadPre path/to/my-vault/**.md",
+        --   "BufNewFile path/to/my-vault/**.md",
+        -- },
+        dependencies = {
+            -- Required.
+            "nvim-lua/plenary.nvim",
+            -- see below for full list of optional dependencies 👇
+        },
+        opts = {
+            workspaces = {
+                {
+                    name = "work",
+                    path = "~/.obsidian/Notes",
+                },
+                {
+                    name = "private",
+                    path = "~/Private/PriObsidian",
+                },
+            },
+            -- see below for full list of options 👇
         },
     },
     { "lepture/vim-jinja" },
