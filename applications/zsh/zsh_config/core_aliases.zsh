3835247955fb01f8fed7b5dbec91bc54140b221d
Yehonatan Elentok <elyoni@gmail.com>
2024-03-21 14:59:54 +0200

    [Zsh] Fix alias

--- applications/zsh/zsh_config/core_aliases.zsh
+++ applications/zsh/zsh_config/core_aliases.zsh
@@ -7,8 +7,8 @@ alias l='ls -lah --color=auto'
 alias ls='ls --color=auto'
 alias ll='ls --color=auto'
 
-[ -f $(ranger) ] && alias rr="ranger"
-[ -f $(gitui) ] && alias gi="gitui"
+[ -f $(which ranger) ] && alias rr="ranger"
+[ -f $(which gitui) ] && alias gi="gitui"
 
 alias clp='xclip -sel clip'
 alias pwdcp='pwd | xclip -sel clip'
