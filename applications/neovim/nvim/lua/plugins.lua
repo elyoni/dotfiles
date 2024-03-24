f726a455fdf7c2ebdda0c0495b6203da4b8e4c98
Yehonatan Elentok <elyoni@gmail.com>
2024-03-24 11:33:04 +0200

    [Nvim] Remove copilot chat plugin

--- applications/neovim/nvim/lua/plugins.lua
+++ applications/neovim/nvim/lua/plugins.lua
@@ -715,35 +715,6 @@ require("lazy").setup({
         end,
 
     },
-    {
-        "jellydn/CopilotChat.nvim",
-        opts = {
-            show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
-            debug = false,     -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
-        },
-        build = function()
-            vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
-        end,
-        event = "VeryLazy",
-        keys = {
-            { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-            { "<leader>cct", "<cmd>CopilotChatTests<cr>",   desc = "CopilotChat - Generate tests" },
-            {
-                "<leader>ccv",
-                ":CopilotChatVisual",
-                mode = "x",
-                desc = "CopilotChat - Open in vertical split",
-            },
-            {
-                "<leader>ccx",
-                ":CopilotChatInPlace<cr>",
-                mode = "x",
-                desc = "CopilotChat - Run in-place code",
-            },
-        },
-    },
-
-
 
 }
 )
