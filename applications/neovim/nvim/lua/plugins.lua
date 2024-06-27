7346db6a952fbed7c25bd09f3cb6d02a33299715
Yehonatan Elentok <elyoni@gmail.com>
2024-06-27 08:51:46 +0300

    [Nvim] clean up Harpoon keybinds

--- applications/neovim/nvim/lua/plugins.lua
+++ applications/neovim/nvim/lua/plugins.lua
@@ -755,14 +755,13 @@ require("lazy").setup({
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
         dependencies = { "nvim-lua/plenary.nvim" },
+        event = "BufEnter",
         config = function()
             local harpoon = require("harpoon")
 
             -- REQUIRED
             harpoon:setup()
-            -- REQUIRED
-
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
         end,
     },
