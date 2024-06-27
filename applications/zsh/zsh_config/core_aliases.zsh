6668b67df742ce041616013663aa7438b0cfcce3
Yehonatan Elentok <elyoni@gmail.com>
2024-06-27 09:45:28 +0300

    [Zsh] Replace which with command for better compatibility

--- applications/zsh/zsh_config/core_aliases.zsh
+++ applications/zsh/zsh_config/core_aliases.zsh
@@ -7,8 +7,8 @@ alias l='ls -lah --color=auto'
 alias ls='ls --color=auto'
 alias ll='ls --color=auto'
 
-[ -f $(which ranger) ] && alias rr="ranger"
-[ -f $(which gitui) ] && alias gi="gitui"
+command -v ranger > /dev/null && alias gi="ranger"
+command -v gitui > /dev/null && alias gi="gitui"
 
 alias clp='xclip -sel clip'
 alias pwdcp='pwd | xclip -sel clip'
