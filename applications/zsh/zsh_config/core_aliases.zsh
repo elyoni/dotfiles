0ceabb762e3e1d7536438c752f2bf5d6d0fbf727
Yehonatan Elentok <elyoni@gmail.com>
2024-06-27 14:00:59 +0300

    [Zsh] Fix ranger alias

--- applications/zsh/zsh_config/core_aliases.zsh
+++ applications/zsh/zsh_config/core_aliases.zsh
@@ -7,7 +7,7 @@ alias l='ls -lah --color=auto'
 alias ls='ls --color=auto'
 alias ll='ls --color=auto'
 
-command -v ranger > /dev/null && alias gi="ranger"
+command -v ranger > /dev/null && alias rr="ranger"
 command -v gitui > /dev/null && alias gi="gitui"
 
 alias clp='xclip -sel clip'
