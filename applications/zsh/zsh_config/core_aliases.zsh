fe5d31c8703b736ab3ff78accadfe589c64b3dd5
Yehonatan Elentok <elyoni@gmail.com>
2024-09-24 10:40:33 +0300

    [Zsh] Alias, jump to dotfiles

--- applications/zsh/zsh_config/core_aliases.zsh
+++ applications/zsh/zsh_config/core_aliases.zsh
@@ -6,6 +6,8 @@ alias ....='cd ../../..'
 alias l='ls -lah --color=auto'
 alias ls='ls --color=auto'
 alias ll='ls --color=auto'
+alias dotf='cd ${HOME}/.dotfiles'
+alias dotfiles='cd ${HOME}/.dotfiles'
 
 command -v ranger > /dev/null && alias rr="ranger"
 command -v gitui > /dev/null && alias gi="gitui"
