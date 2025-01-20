b403c627cf1f6a7b30656932c37ef06e01c153dd
Yehonatan Elentok <elyoni@gmail.com>
2025-01-20 09:41:07 +0200

    [Nvim] Add LSP keybinds

--- applications/neovim/nvim/lua/plugins/lsp.lua
+++ applications/neovim/nvim/lua/plugins/lsp.lua
@@ -19,6 +19,7 @@ return
                 },
                 mapping = cmp.mapping.preset.insert({
                     ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
                 }),
