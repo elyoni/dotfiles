6db9dbbd9a505202a551491c69ced1718e552b85
Yehonatan Elentok <elyoni@gmail.com>
2025-03-18 09:57:28 +0200

    [Nvim] Re enable NvimTree focus

--- applications/neovim/nvim/lua/plugins/nvimtree.lua
+++ applications/neovim/nvim/lua/plugins/nvimtree.lua
@@ -14,7 +14,7 @@ return {
                 },
                 auto_reload_on_write = true,
                 update_focused_file = {
-                    enable = false,
+                    enable = true,
                     update_root = {
                         enable = false,
                         ignore_list = {},
