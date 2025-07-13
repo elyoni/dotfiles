5920233eb73057f0fb2c2e3313508563951fc27e
Yoni Elentok <elyoni@gmail.com>
2025-07-13 09:22:25 +0300

    [Regolith] Update i3xrocks configuration

--- system/regolith/regolith3/i3/terminal.sh
+++ system/regolith/regolith3/i3/terminal.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ -f "$(which wezterm)" ]]; then
+if [[ -f "$(which ghostty)" ]]; then
+    ghostty "$@"
+elif [[ -f "$(which gnome-terminal)" ]]; then
+    gnome-terminal "$@"
+elif [[ -f "$(which wezterm)" ]]; then
     wezterm "$@"
 elif [[ -f "$(which kitty)" ]]; then
     kitty "$@"
-elif [[ -f "$(which gnome-terminal)" ]]; then
-    gnome-terminal "$@"
 fi
--- system/regolith/regolith3/i3xrocks/scripts/disable_laptop_keyboard.sh
+++ system/regolith/regolith3/i3xrocks/scripts/disable_laptop_keyboard.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 
-DEVICE_ID=$(xinput list | grep -E "AT Translated Set 2 keyboard|Another Device|Some Other Device" | awk -F'id=' '{print $2}' | awk '{print $1}')
+DEVICE_KEYBOARD_ID=$(xinput list | grep -E "AT Translated Set 2 keyboard|Another Device|Some Other Device" | awk -F'id=' '{print $2}' | awk '{print $1}')
 
-if xinput list-props $DEVICE_ID | grep "Device Enabled (.*):.*1" > /dev/null
+DEVICE_TRACKPAD_ID=$(xinput list | grep -E "TrackPoint" | awk -F'id=' '{print $2}' | awk '{print $1}')
+if xinput list-props $DEVICE_KEYBOARD_ID | grep "Device Enabled (.*):.*1" > /dev/null
 then
-    xinput disable $DEVICE_ID
+    xinput disable $DEVICE_KEYBOARD_ID
+    xinput disable $DEVICE_TRACKPAD_ID
     echo "Keyboard disabled"
 else
-    xinput enable $DEVICE_ID
+    xinput enable $DEVICE_KEYBOARD_ID
+    xinput enable $DEVICE_TRACKPAD_ID
     echo "Keyboard enabled"
 fi
