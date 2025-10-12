ce08c1029ace09cd92010aa163e230d25a48b47b
Yoni Elentok <elyoni@gmail.com>
2025-10-12 14:23:39 +0300

    [Install] Update inst. script order

--- install_scripts/03terminal_apps_install.sh
+++ install_scripts/03terminal_apps_install.sh
@@ -6,18 +6,19 @@ source "${DIR}"/09common
 
 summary=""
 
-install_package "Ripgrep" "${DIR}"/../applications/ripgrep
+# Install ZSH first as other apps depend on directories it creates (.zfunc, etc.)
+install_package "Zsh" "${DIR}"/../applications/zsh
 install_package "Git" "${DIR}"/../applications/git
 install_package "Neovim" "${DIR}"/../applications/neovim
+install_package "Tmux" "${DIR}"/../applications/tmux
+install_package "Ripgrep" "${DIR}"/../applications/ripgrep
 install_package "Fzf" "${DIR}"/../applications/fzf
-install_package "Zsh" "${DIR}"/../applications/zsh
 install_package "tig" "${DIR}"/../applications/tig
 install_package "Ranger" "${DIR}"/../applications/ranger
 install_package "Cheat" "${DIR}"/../applications/cheat
 install_package "Direnv" "${DIR}"/../applications/direnv
 install_package "Bat" "${DIR}"/../applications/bat
 install_package "Docker" "${DIR}"/../applications/docker
-install_package "Tmux" "${DIR}"/../applications/tmux
 install_package "Tusk" "${DIR}"/../applications/tusk
 
 
