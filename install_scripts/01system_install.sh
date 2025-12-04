52333244422fda336073d6ec1679b3d26399af2f
Yoni Elentok <elyoni@gmail.com>
2025-12-04 12:21:08 +0200

    [Install] Add AppImage, Flatpak, Bluetui, and Pulsemixer to system apps

--- install_scripts/01system_install.sh
+++ install_scripts/01system_install.sh
@@ -11,6 +11,10 @@ install_package "Fonts" "${DIR}"/../system/fonts/
 install_package "Bluetooth" "${DIR}"/../system/bluetooth/
 install_package "Network" "${DIR}"/../system/network/
 install_package "SSH" "${DIR}"/../system/ssh/
+install_package "AppImage" "${DIR}"/../system/appimage
+install_package "Flatpak" "${DIR}"/../system/flatpak
+install_package "Bluetui" "${DIR}"/../system/tui_application/bluetui
+install_package "Pulsemixer" "${DIR}"/../system/tui_application/pulsemixer
 
 
 echo "${summary}"
