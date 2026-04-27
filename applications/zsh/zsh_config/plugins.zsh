d3438f500b1d3e08159383ccd4774c5cc9d20ce3
Yoni Elentok <elyoni@gmail.com>
2026-04-27 15:38:34 +0300

    fix - zsh - use CURSOR_AGENT to scope simple prompt to chat terminal only

--- applications/zsh/zsh_config/plugins.zsh
+++ applications/zsh/zsh_config/plugins.zsh
@@ -2,7 +2,7 @@ source "${ZSH_CUSTOM_PLUGIN}"/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # Temporarily testing Starship instead of P10k
 # Optimized: cache starship init to avoid subprocess on every shell start
-if [[ "$TERM_PROGRAM" != "vscode" ]]; then
+if [[ "$CURSOR_AGENT" != "1" ]]; then
     # source ~/.powerlevel10k/powerlevel10k.zsh-theme
     local starship_cache="${XDG_CACHE_HOME:-$HOME/.cache}/starship_init.zsh"
     if [[ ! -f "$starship_cache" || "$starship_cache" -ot "${commands[starship]}" ]]; then
