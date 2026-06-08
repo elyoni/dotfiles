7c326164386780336bf534174a7812b6232cea04
Yoni Elentok <elyoni@gmail.com>
2026-06-08 14:16:18 +0300

    feat(zsh): add kubectl completion caching

--- applications/zsh/zsh_config/kubectl_aliases.zsh
+++ applications/zsh/zsh_config/kubectl_aliases.zsh
@@ -1,4 +1,14 @@
+# Load kubectl completion (cached to avoid slow startup)
+if (( $+commands[kubectl] )); then
+    local kubectl_cache="${XDG_CACHE_HOME:-$HOME/.cache}/kubectl_completion.zsh"
+    if [[ ! -f "$kubectl_cache" || "$kubectl_cache" -ot "${commands[kubectl]}" ]]; then
+        kubectl completion zsh > "$kubectl_cache"
+    fi
+    source "$kubectl_cache"
+fi
+
 alias k='kubectl'
+compdef k=kubectl
 alias kl='kubectl logs'
 alias kg='kubectl get'
 alias kgn='kubectl get nodes'
