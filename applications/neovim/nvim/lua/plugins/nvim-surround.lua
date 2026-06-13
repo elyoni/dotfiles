e5e11c7d67087ed69ea04e400e94fff95d709a2f
Yoni Elentok <elyoni@gmail.com>
2026-06-13 10:36:25 +0300

    fix(neovim): use explicit keymaps for nvim-surround

--- applications/neovim/nvim/lua/plugins/nvim-surround.lua
+++ applications/neovim/nvim/lua/plugins/nvim-surround.lua
@@ -3,11 +3,8 @@ return {
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup({
-            keymaps = {
-                visual = "m",
-                normal = "ms",
-            },
-        })
+        require("nvim-surround").setup()
+        vim.keymap.set("x", "m", "<Plug>(nvim-surround-visual)")
+        vim.keymap.set("n", "ms", "<Plug>(nvim-surround-normal)")
     end
 }
